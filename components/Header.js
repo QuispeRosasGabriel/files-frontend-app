b1b5360994f489fc82685b34f05374a1881f2fb0
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-12-13 11:58:59 -0500

    creando cuenta

--- components/Header.js
+++ components/Header.js
@@ -4,14 +4,16 @@ import Link from 'next/link';
 const Header = () => {
     return (
         <header className="py-8 flex flex-col md:flex-row items-center justify-between">
-            <img src="logo.svg" className="w-64 mb-8 md:mb-0"/>
+            <Link href="/">
+            <img src="logo.svg" className="w-64 cursor-pointer mb-8 md:mb-0"/>
+            </Link>
 
             <div>
                 <Link href="/login">
-                    <a className="bg-red-500 px-5 py-3 rounded text-white font-bold uppercase">Iniciar Sesión</a>
+                    <a className="bg-red-500 cursor-pointer mr-2 px-5 py-3 rounded text-white font-bold uppercase">Iniciar Sesión</a>
                 </Link>
                 <Link href="/crearcuenta">
-                    <a className="bg-red-500 px-5 py-3 rounded text-white font-bold uppercase">Crear Cuenta</a>
+                    <a className="bg-black cursor-pointer px-5 py-3 rounded text-white font-bold uppercase">Crear Cuenta</a>
                 </Link>
             </div>
         </header>
--- pages/crearcuenta.js
+++ pages/crearcuenta.js
@@ -4,7 +4,17 @@ import Layout from '../components/Layout';
 const CrearCuenta = () => {
   return (
     <Layout>
-      <h1>CrearCuenta</h1>
+        <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
+            <h2 className="text-4xl font-sans font-bold text-gray-800 text-center my-4">Crear Cuenta</h2>
+
+            <div className="flex justify-center mt-5">
+                <div className="w-full max-w-lg">
+                    <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
     </Layout>
   )
 }
