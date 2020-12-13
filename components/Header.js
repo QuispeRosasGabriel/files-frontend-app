5bb67f275d10b31d141c830bc4a2fcd17128aa28
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-12-13 11:51:25 -0500

    header js

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Header = () => {
     return (
         <header className="py-8 flex flex-col md:flex-row items-center justify-between">
             <img src="logo.svg" className="w-64 mb-8 md:mb-0"/>
+
+            <div>
+                <Link href="/login">
+                    <a className="bg-red-500 px-5 py-3 rounded text-white font-bold uppercase">Iniciar Sesión</a>
+                </Link>
+                <Link href="/crearcuenta">
+                    <a className="bg-red-500 px-5 py-3 rounded text-white font-bold uppercase">Crear Cuenta</a>
+                </Link>
+            </div>
         </header>
     )
 }
---
+++ pages/crearcuenta.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Layout from '../components/Layout';
+
+const CrearCuenta = () => {
+  return (
+    <Layout>
+      <h1>CrearCuenta</h1>
+    </Layout>
+  )
+}
+
+export default CrearCuenta;
