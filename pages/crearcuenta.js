b5d53692ee35a2c5124133e02c31bf2d357f379e
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-12-13 21:55:42 -0500

    integrando formik y yup

--- pages/crearcuenta.js
+++ pages/crearcuenta.js
@@ -1,7 +1,27 @@
 import Head from 'next/head'
 import Layout from '../components/Layout';
+import { useFormik } from "formik";
+import * as Yup from 'yup';
 
 const CrearCuenta = () => {
+
+    // VALIDANDO CON FORMIK Y YUP
+    const formik = useFormik({
+        initialValues: {
+            nombre: '',
+            email: '',
+            password: ''
+        },
+        validationSchema: Yup.object({
+            nombre: Yup.string().required('El nombre es obligatorio'),
+            email: Yup.string().email('El email no es valido').required('El email es obligatorio'),
+            password: Yup.string().required('El password es obligatorio').min(6, 'El password debe contener al menos 6 caracteres')
+        }),
+        onSubmit: (values) => {
+            console.log('Enviando formulario', values);
+        }
+    });
+
     return (
         <Layout>
             <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
@@ -9,7 +29,9 @@ const CrearCuenta = () => {
 
                 <div className="flex justify-center mt-5">
                     <div className="w-full max-w-lg">
-                        <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+                        <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+                            onSubmit={formik.handleSubmit}
+                        >
                             <div className="mb-4">
                                 <label className="block text-black text-sm font-bold mb-2"
                                     htmlFor="nombre"
@@ -18,7 +40,16 @@ const CrearCuenta = () => {
                             rounded w-full py-2 px-3 text-gray-700 leading-light focus:outline-none focus:shadow-outline"
                                     id="nombre"
                                     placeholder="Nombre de usuario"
+                                    value={formik.values.nombre}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
                                 />
+                                {formik.touched.nombre && formik.errors.nombre ? (
+                                    <div className="my-2 bg-gray-200 border-l-4 border-red-500 text-red-700 p-4">
+                                        <p className="font-bold">Error</p>
+                                        <p>{formik.errors.nombre}</p>
+                                    </div>
+                                ) : null}
                             </div>
 
                             <div className="mb-4">
@@ -29,7 +60,16 @@ const CrearCuenta = () => {
                             rounded w-full py-2 px-3 text-gray-700 leading-light focus:outline-none focus:shadow-outline"
                                     id="email"
                                     placeholder="Nombre de email"
+                                    value={formik.values.email}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
                                 />
+                                 {formik.touched.email && formik.errors.email ? (
+                                    <div className="my-2 bg-gray-200 border-l-4 border-red-500 text-red-700 p-4">
+                                        <p className="font-bold">Error</p>
+                                        <p>{formik.errors.email}</p>
+                                    </div>
+                                ) : null}
                             </div>
 
                             <div className="mb-4">
@@ -40,7 +80,16 @@ const CrearCuenta = () => {
                             rounded w-full py-2 px-3 text-gray-700 leading-light focus:outline-none focus:shadow-outline"
                                     id="password"
                                     placeholder="Ingresa tu contraseña"
+                                    value={formik.values.password}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
                                 />
+                                     {formik.touched.password && formik.errors.password ? (
+                                    <div className="my-2 bg-gray-200 border-l-4 border-red-500 text-red-700 p-4">
+                                        <p className="font-bold">Error</p>
+                                        <p>{formik.errors.password}</p>
+                                    </div>
+                                ) : null}
                             </div>
 
                             <input type="submit"
