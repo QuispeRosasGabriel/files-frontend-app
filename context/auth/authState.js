7669763c7cb49062c50dcb181406d561ef4bec6e
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-12-14 21:36:23 -0500

    use context para el state

---
+++ context/auth/authState.js
@@ -0,0 +1,14 @@
+import React, {useReducer} from 'react';
+import authContext from "./authContext";
+
+const AuthState = ({children}) => {
+    return (
+        <authContext.Provider
+            value={{}}
+        >
+            {children}
+        </authContext.Provider>
+    )
+}
+
+export default AuthState;
