56d3386f48ec8735b6ec1c0ab2a8dc0bf029cd92
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-12-19 11:47:44 -0500

    agregando dropzone

--- components/Dropzone.js
+++ components/Dropzone.js
@@ -3,10 +3,15 @@ import { useDropzone } from 'react-dropzone';
 import clienteAxios from '../config/axios';
 
 const DropZone = () => {
+
+    const { getRootProps, getInputProps } = useDropzone();
+
     return (
         <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0 
         flex flex-col items-center justify-center border-dashed border-gray-400 border-2 bg-gray-100">
-            AC
+            <div {...getRootProps({className: 'dropzone w-full py-32'})}>
+                <input className="h-100" {...getInputProps()} />
+            </div>
         </div>
     )
 }
