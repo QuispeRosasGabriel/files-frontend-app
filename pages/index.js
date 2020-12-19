cd882cee259dac2ce60a1cb4577cc96ed52e0f0f
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-12-19 10:57:24 -0500

    layout de dropzone

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { useContext, useEffect } from 'react';
 import Layout from '../components/Layout';
 import authContext from '../context/auth/authContext';
@@ -15,9 +16,22 @@ const Index = () => {
     <Layout>
       <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
         <div className="lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10">
-            aca
           <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0">
-          
+            <p>ACA</p>
+          </div>
+          <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0">
+            <h2 className="text-4xl font-sans font-bold text-gray-800 my-4">
+              Compartir archivos de forma sencilla y privada
+            </h2>
+            <p className="text-lg leading-loose">
+              <span className="text-red-500 font-bold">Node Send with React </span>
+              Te permite crear archivos con cifrado de extremo a extremo, por Gabriel Quispe Rosas
+            </p>
+            <Link href="/crearcuenta">
+              <a className="text-red-500 font-bold text-lg hover:text-red-700">
+                Crea una cuenta para mayores beneficios
+              </a>
+            </Link>
           </div>
         </div>
       </div>
