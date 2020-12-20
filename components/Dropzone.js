b2bfbb12a44d751f8e6604f49abfb1b2876e08db
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-12-19 20:09:27 -0500

    use callback

--- components/Dropzone.js
+++ components/Dropzone.js
@@ -4,7 +4,14 @@ import clienteAxios from '../config/axios';
 
 const DropZone = () => {
 
-    const { getRootProps, getInputProps, isDragActive } = useDropzone();
+    const { getRootProps, getInputProps, isDragActive, acceptedFiles } = useDropzone({ onDrop });
+
+    const onDrop = useCallback(
+        (acceptedFiles) => {
+            console.log(acceptedFiles);
+        }
+    )
+
 
     return (
         <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0 
