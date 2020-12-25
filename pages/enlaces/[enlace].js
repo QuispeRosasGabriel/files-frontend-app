8f89f5713dab368cacee1ede1751408010b6c330
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-12-25 14:46:59 -0500

    enlace dinamico

--- pages/enlaces/[enlace].js
+++ pages/enlaces/[enlace].js
@@ -2,14 +2,24 @@ import Layout from '../../components/Layout';
 import clienteAxios from '../../config/axios';
 
 export async function getStaticProps() {
-
+    const resultado = await clienteAxios.get('/api/enlaces/');
+    return {
+        props: {
+            enlace: resultado.data
+        }
+    }
 }
 
 export async function getStaticPaths() {
-    
+    const enlaces = await clienteAxios.get('/api/enlaces');
+    return {
+        paths: enlaces?.data?.enlaces?.
+        map((enlace) => ({ params: {enlace: enlace?.url} })),
+        fallback: false
+    }
 }
 
-export default = () => {
+export default ({enlace}) => {
     return(
         <Layout>
             <h1>Desde [enlace].js</h1>
